7a3289a3b0b9f24bf957bdd2207c609341fce59d
Cintia Zago <cintiazago@gmail.com>
2019-03-24 13:53:40 -0300

    Firset commit

---
+++ console_app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using GameTOP.Lib;
+
+namespace console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jogo = new JogoFODA(
+                new Jogador1("Ronaldo"),
+                new Jogador2()
+            );
+            jogo.IniciarJogo();
+        }
+    }   
+}
